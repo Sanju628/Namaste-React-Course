ef42c79b6dfefb13c0b0e9e1163755f3606ca4a9
Sanju628 <sanjubaloria12@gmail.com>
2023-10-22 23:36:57 +0530

    Episode 03

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Namaste React Course 🚀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR -> Hot Module Replacement
+- File watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimizations
+- Minifications
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support old Browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree Shaking - remove unused Code
+
+
+# JSX 
+- Extensible Javscript syntax
+- It is transplied by the Babel(Parcel) Before reaching to JS Engine
+- Babel Transpile it to -> React.createElement -> ReactElement JS Object -> HTMLElement(render)
+
+# Functional Component 
+- Its just a normal javascript function that just return a ReactElement piece of code or jsx.
--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-{/* <div id="parent">
-    <div id="child1">
-        <h1>hello this is h1</h1>
-        <h2>hello this is h2</h2>
-    </div>
-    <div id="child2">
-        <h1>hello this is h1</h1>
-        <h2>hello this is h2</h2>
-    </div>
-</div> */}
-const heading = React.createElement("div",{id:"parent"},[
-    React.createElement("div", {id:"child1"}, [
-        React.createElement("h1", {}, "hello this is h1"),
-        React.createElement("h2", {}, "hello this is h2"),
-    ]),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "hello this is h1"),
-        React.createElement("h2", {}, "hello this is h2"),
-    ]),
-]);
-// console.log(heading);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const JSXHeading = () => <h1 id="heading">Namaste ReactJs 🚀</h1>
+
+
+
+const ReactComponent = () => (<div>
+        <JSXHeading/>
+        <h1>Welcome to the react ReactComponent</h1>
+    </div>);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<ReactComponent/>);
